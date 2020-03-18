e8e37807d85f45582465a56c125718173f1b81bf
morfinPL <pl.morfin.pl@gmail.com>
2020-03-18 12:17:01 +0100

    Change app to read sampleFaces.

--- apps/FaceDetection/src/main.cpp
+++ apps/FaceDetection/src/main.cpp
@@ -1,5 +1,7 @@
 #include <map>
 #include <string>
+#include <iostream>
+#include <filesystem>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -7,16 +9,61 @@
 
 #include <Configuration.h>
 
-using namespace cv;
-using namespace std;
+namespace
+{
+bool hasEnding(const std::string &fullString, const std::string &ending)
+{
+	if (fullString.size() < ending.length())
+	{
+		return false;
+	}
+	return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
+}
+
+std::vector<std::string> findPNGFromDirectory(const std::string &directory)
+{
+	std::vector<std::string> pngs;
+	for (const auto &p : std::filesystem::directory_iterator(directory))
+	{
+		if (p.is_regular_file() && hasEnding(p.path().filename().string(), ".png"))
+		{
+			pngs.emplace_back(p.path().string());
+		}
+	}
+	return pngs;
+}
+
+std::vector<cv::Mat> readGrayImages(const std::vector<std::string> &paths)
+{
+	std::vector<cv::Mat> images;
+	for (const auto &path : paths)
+	{
+		cv::Mat image = cv::imread(path);
+		if (image.channels() == 3)
+		{
+			cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+		}
+		images.emplace_back(image);
+	}
+	return images;
+}
+} // namespace
 
 int main(const int argc, const char **argv)
 {
 	const auto params = Configuration::parseParameters(argc, argv);
-	Mat image = Mat::zeros(300, 600, CV_8UC3);
-	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
-	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
-	imshow("Display Window", image);
-	waitKey(0);
+	const auto sampleFacesDir = "..\\..\\..\\testData\\sampleFaces";
+	const auto imagePaths = findPNGFromDirectory(sampleFacesDir);
+	std::cout << "The following images are founded:" << std::endl;
+	for (const auto &path : imagePaths)
+	{
+		std::cout << path << std::endl;
+	}
+	const auto images = readGrayImages(imagePaths);
+	for (const auto &image : images)
+	{
+		cv::imshow("Image", image);
+		cv::waitKey();
+	}
 	return 0;
 }
