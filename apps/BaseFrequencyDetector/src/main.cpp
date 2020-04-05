6b8fd00db6ade76a48ce0b4fe73e4bbd29a758a4
Filip Pietrusiak <filip.pietrusiak@gmail.com>
2020-04-05 21:19:09 +0200

    Modify app to detect base frequencies.

--- apps/BaseFrequencyDetector/src/main.cpp
+++ apps/BaseFrequencyDetector/src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
+#include <AudioFile.h>
+
 #include <Configuration.h>
 #include <IO.h>
-
-#include <AudioFile.h>
+#include <SoundProcessing.h>
 
 int main(const int argc, const char **argv)
 {
@@ -11,17 +12,36 @@ int main(const int argc, const char **argv)
     if (params.empty() || params.count("--help") != 0 || params.count("-h"))
     {
         std::string help = "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<        HELP        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
-        help += "--input - path of input wav file, required\n";
-        help += "--output - path of output txt file, required\n";
+        help += "--input - path of input, required\n";
+        help += "--output - path of output, optional\n";
+        help += "--window - size of window, required\n";
+        help += "--treshold - threshold for fourier analysis, required\n";
         std::cout << help;
         return 0;
     }
     const auto inputPath(params.at("--input"));
     const auto outputPath(params.at("--output"));
+    const auto N(std::stoi(params.at("--window")));
+    const auto treshold(std::stod(params.at("--treshold")));
 
     AudioFile<double> input;
     input.load(inputPath);
     input.printSummary();
-	std::cout << "Number of samples: " << input.samples.front().size() << "\n";
-	IO::saveWavToTxt(outputPath, input);
+    IO::saveWavToTxt(outputPath + "Orginal.txt", input);
+    std::cout << "Autocorelation" << std::endl;
+    auto result = SoundProcessing::autoCorelation(input, N);
+    for (auto i = 0; i < static_cast<int>(result.first.size()); i++)
+    {
+        std::cout << result.first[i] << "[Hz] " << std::endl;
+    }
+    IO::saveWavToTxt(outputPath + ".txt", result.second);
+    result.second.save(outputPath + ".wav");
+    std::cout << "Fourier" << std::endl;
+    auto fourierResult = SoundProcessing::fourier(input, N, treshold);
+    IO::saveWavToTxt(outputPath + "fourier.txt", fourierResult.second);
+    fourierResult.second.save(outputPath + "fourier.wav");
+    for (auto i = 0; i < static_cast<int>(fourierResult.first.size()); i++)
+    {
+        std::cout << fourierResult.first[i] << "[Hz] " << std::endl;
+    }
 }
